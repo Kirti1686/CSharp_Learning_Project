11b020b8df96bb4af119410f2b753bdfa7607a5c
Kirti1686 <kirti.sinha16@gmail.com>
2021-10-26 12:18:00 +0530

    Added Dashboard Page

--- AutoTestFramework/Pages/LoginPage.cs
+++ AutoTestFramework/Pages/LoginPage.cs
@@ -22,7 +22,7 @@ namespace AutoTestFramework.Pages
         public IWebElement UsernameTxt => driver.FindElement(By.Id("username"));
         public IWebElement PasswordTxt => driver.FindElement(By.Id("password"));
         public IWebElement LogInbtn => driver.FindElement(By.Id("btn-login"));
-        private IWebElement DisplayName => driver.FindElement(By.XPath("//span[text()='Good Evening, Tyron!']"));
+        private IWebElement DisplayNamelbl => driver.FindElement(By.XPath("//span[text()='Good Evening, Tyron!']"));
 
         public void Login()
         {
@@ -32,7 +32,7 @@ namespace AutoTestFramework.Pages
         }
         public bool IsLoginSuccessful()
         {
-            return DisplayName.Text.Equals("Good Evening, Tyron!");
+            return DisplayNamelbl.Text.Equals("Good Evening, Tyron!");
         }
     }
 }
